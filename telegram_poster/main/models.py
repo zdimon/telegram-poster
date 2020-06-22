ea448ad2e5b99f4172c73f0af53a337e19a64eff
Dmitry Zharikov <zdimon77@gmail.com>
2020-06-22 12:34:26 +0300

    commit from zdimon77@gmail.com ----

--- telegram_poster/main/models.py
+++ telegram_poster/main/models.py
@@ -3,7 +3,10 @@ from django.db import models
 class Client(models.Model):
     name = models.CharField(max_length=250, db_index=True)
     secret = models.CharField(max_length=250, db_index=True)
+    def __str__(self):
+        return self.name
 
 class TelegramUser(models.Model):
     key = models.CharField(max_length=250, db_index=True)
+    name = models.CharField(max_length=250, db_index=True)
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
